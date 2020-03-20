57bf18b105d569d0b576af4b12c6e00879eeeef3
Charlie Weems <charlie.weems@gmail.com>
2020-03-20 15:45:26 -0700

    Fix tests

--- src/state/usePasscodeAuth/usePasscodeAuth.ts
+++ src/state/usePasscodeAuth/usePasscodeAuth.ts
@@ -5,22 +5,22 @@ import queryString from 'query-string';
 export function getRoomName() {
   const parsed = queryString.parse(window.location.search);
   const room = parsed.room ? parsed.room : window.sessionStorage.getItem('room');
-  return room;
+  return room ? String(room) : null;
 }
 
 export function getUserName() {
   const parsed = queryString.parse(window.location.search);
   const user = parsed.user ? parsed.user : window.sessionStorage.getItem('user');
-  return user;
+  return user ? String(user) : null;
 }
 
 export function getPasscode() {
   const parsed = queryString.parse(window.location.search);
   const passcode = parsed.passcode ? parsed.passcode : window.sessionStorage.getItem('passcode');
-  return passcode;
+  return passcode ? String(passcode) : null;
 }
 
-export function fetchToken(name: string, room: string, passcode: string) {
+export function fetchToken(name: string, room: string, passcode: string | string[]) {
   return fetch(`/token`, {
     method: 'POST',
     headers: {
@@ -30,7 +30,7 @@ export function fetchToken(name: string, room: string, passcode: string) {
   });
 }
 
-export function verifyPasscode(passcode: string) {
+export function verifyPasscode(passcode: string | string[]) {
   return fetchToken('temp-name', 'temp-room', passcode).then(async res => {
     const jsonResponse = await res.json();
     if (res.status === 401) {
